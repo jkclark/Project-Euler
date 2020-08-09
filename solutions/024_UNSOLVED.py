0ba5c047d01c0b0ff35f7dbcfa36428ba9034ea7
Josh Clark <jclarktennis@gmail.com>
2020-08-08 22:42:47 -0400

    Created files for problems 23 and 24.

---
+++ solutions/024_UNSOLVED.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of
+the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it
+lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+    012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+'''
+
+from helpers import print_memory_usage_report, print_time_elapsed
+from time import time
+import tracemalloc
+
+
+def main():
+    # Keep track of time elapsed and memory used
+    start_time = time()
+    tracemalloc.start()
+
+    # ********** Solution begins here ********** #
+
+    print(f':\n\n\t{None}\n')
+
+    # ********** Solution ends here ********** #
+
+    # Stop tracking time and memory
+    snapshot = tracemalloc.take_snapshot()
+    end_time = time()
+    tracemalloc.stop()
+
+    # Print time elapsed and memory used
+    print_time_elapsed(start_time, end_time)
+    print_memory_usage_report(snapshot)
+
+
+if __name__ == '__main__':
+    main()
