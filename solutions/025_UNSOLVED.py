a7772d679b7c835c52d9ddbb6535e4e1320a2fb8
Josh Clark <jclarktennis@gmail.com>
2020-08-15 16:50:16 -0400

    Solved problem 25.

---
+++ solutions/025_UNSOLVED.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+'''
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+'''
+
+from helpers import print_memory_usage_report, print_time_elapsed
+from time import time
+import tracemalloc
+
+
+def main():
+    # Keep track of time elapsed and memory used
+    start_time = time()
+    tracemalloc.start()
+
+    # ********** Solution begins here ********** #
+
+    a = 1
+    b = 1
+    c = 2
+    index = 2  # F3 = 2 is the first number we'll calculate in the loop
+    while len(str(c)) < 1000:
+        c = a + b
+        a = b
+        b = c
+
+        index += 1
+
+    print(f'The index of the first term in the Fibonacci sequence with 1000 digits:\n\n\t{index}\n')
+
+    # ********** Solution ends here ********** #
+
+    # Stop tracking time and memory
+    snapshot = tracemalloc.take_snapshot()
+    end_time = time()
+    tracemalloc.stop()
+
+    # Print time elapsed and memory used
+    print_time_elapsed(start_time, end_time)
+    print_memory_usage_report(snapshot)
+
+
+if __name__ == '__main__':
+    main()
