f82ae891ced29e59dbb651b442ff0c4b2bc77157
Josh Clark <jclarktennis@gmail.com>
2022-05-16 13:48:20 -0400

    Solved problem 28.

---
+++ solutions/028.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+'''
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+    21 22 23 24 25
+    20  7  8  9 10
+    19  6  1  2 11
+    18  5  4  3 12
+    17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+'''
+
+from helpers import print_memory_usage_report, print_time_elapsed
+from time import time
+import tracemalloc
+
+
+def main():
+    # Keep track of time elapsed and memory used
+    start_time = time()
+    tracemalloc.start()
+
+    # ********** Solution begins here ********** #
+
+    MAX = 1001 * 1001
+    current = 1
+    diagonals_total = 1
+    skip_size = 2
+    while current < MAX:
+        for _ in range(4):
+            current += skip_size
+            diagonals_total += current
+
+        skip_size += 2
+
+    print(f'The sum of the numbers on the diagonals in a 1001 by 1001 spiral:\n\n\t{diagonals_total}\n')
+
+    # ********** Solution ends here ********** #
+
+    # Stop tracking time and memory
+    snapshot = tracemalloc.take_snapshot()
+    end_time = time()
+    tracemalloc.stop()
+
+    # Print time elapsed and memory used
+    print_time_elapsed(start_time, end_time)
+    print_memory_usage_report(snapshot)
+
+
+if __name__ == '__main__':
+    main()
