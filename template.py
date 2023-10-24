e3349e07d5f254d8e641398639ee5e024c0d9893
Josh Clark <jclarktennis@gmail.com>
2023-10-24 14:51:44 +0200

    refactor: use new time/space measuring decorator

--- template.py
+++ template.py
@@ -1,33 +1,15 @@
 #!/usr/bin/env python3
-'''
+"""
 
-'''
+"""
 
-from helpers import print_memory_usage_report, print_time_elapsed
-from time import time
-import tracemalloc
+from helpers import measure_time_and_memory
 
 
+@measure_time_and_memory
 def main():
-    # Keep track of time elapsed and memory used
-    start_time = time()
-    tracemalloc.start()
-
-    # ********** Solution begins here ********** #
-
-    print(f':\n\n\t{None}\n')
-
-    # ********** Solution ends here ********** #
-
-    # Stop tracking time and memory
-    snapshot = tracemalloc.take_snapshot()
-    end_time = time()
-    tracemalloc.stop()
-
-    # Print time elapsed and memory used
-    print_time_elapsed(start_time, end_time)
-    print_memory_usage_report(snapshot)
+    print(f":\n\n\t{None}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
