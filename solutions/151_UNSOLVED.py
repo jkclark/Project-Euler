4bba27a781ab3548d1dda3df0facf031b2a0e94d
Josh Clark <jclarktennis@gmail.com>
2023-11-22 14:57:45 -0500

    Solved problem 151

--- solutions/151_UNSOLVED.py
+++ solutions/151_UNSOLVED.py
@@ -27,7 +27,7 @@ the envelope.
 
 Give your answer rounded to six decimal places using the format x.xxxxxx .
 """
-from typing import List
+from collections import Counter
 
 from helpers import measure_time_and_memory
 
@@ -43,54 +43,106 @@ def main():
     there. The probability of getting to any given node is the product of the
     probabilities of getting to all of that node's parents.
     """
+    # Memoization
+    # Here were storing envelope states and their corresponding values
+    # The value of any envelope state is the sum of the values of all children
+    # envelopes
+    envelopes_to_probabilities = {}
 
-    total = 0
-
-    # NOTE: add probability as parameter, starting at 1
-    #       one potential point of inaccuracy/imprecision is multiplying
-    #       floats together. one option is to just multiply the denominator
-    #       through recursive calls as an integer, and just dividing it at the end
-    def dfs(envelope: List[int], probability: float, running_total: int):
+    def dfs(envelope: Counter, level: int):
         """Run a depth-first search from this envelope state."""
-        nonlocal total
+        nonlocal envelopes_to_probabilities
 
-        # If we only have one piece of paper in the envelope
-        if len(envelope) == 1:
-            # If this is the the last piece of paper in the envelope,
-            # we're done
-            if envelope[0] == 1:
-                total += running_total * probability
-                return
+        # If we've seen this envelope before, we're done
+        fset = convert_counter_to_frozenset(envelope)
+        if fset in envelopes_to_probabilities:
+            return envelopes_to_probabilities[fset]
 
-            # Otherwise, this is the 'only one piece of paper' case, so add
-            # to running total
-            running_total += 1
+        # Base case: only 1 A5 piece of paper is in the envelope
+        # if envelope.total() == 1:  # Python 3.12 being annoying on Windows machine
+        if sum(envelope.values()) == 1 and 1 in envelope:
+            return 0
 
-        # Recurse through the envelope states available to us, multiplying the
-        # current probability by the probability of giong to that state
-        num_pieces = len(envelope)
+        # Recurse through the envelope states available to us
+        this_envelope_value = 0
         for piece in envelope:
             # Create copy of envelope
-            new_envelope = envelope.copy()
+            child_envelope = envelope.copy()
+
+            # Find the "probability" of choosing this piece
+            this_piece_probability = child_envelope[piece] / sum(
+                child_envelope.values()
+            )
 
             # Remove this piece of paper
-            new_envelope.remove(piece)
+            remove_piece_from_envelope(piece, child_envelope)
 
             # Add newly created pieces of paper
-            piece = piece // 2
-            while piece >= 1:
-                new_envelope.append(piece // 2)
-                piece = piece // 2
+            add_new_pieces_from_piece_to_envelope(piece, child_envelope)
+
+            # Check if child-envelope state has only 1 piece of non-A5 paper
+            child_envelope_has_one_piece = (
+                sum(child_envelope.values()) == 1 and 1 not in child_envelope
+            )
 
             # Recurse
-            dfs(new_envelope, probability * (1 / num_pieces), running_total)
+            child_values = dfs(child_envelope, level + 1)
 
-    dfs([8, 8], 1, 0)
+            # The value for this child envelope state is:
+            # (probability of choosing a piece of this size) * (
+            #   (1 if there is only one piece of paper in this child envelope, 0 otherwise)
+            #   +
+            #   (the EV for this child-envelope state)
+            # )
+            this_envelope_value += this_piece_probability * (
+                child_envelope_has_one_piece + child_values
+            )
 
+        # Memoize
+        envelopes_to_probabilities[fset] = this_envelope_value
+
+        return this_envelope_value
+
+    initial_envelope = Counter([16])
+    dfs(initial_envelope, 0)
+
+    answer = round(
+        envelopes_to_probabilities[convert_counter_to_frozenset(initial_envelope)], 6
+    )
     print(
         f"The expected number of times a single piece of paper is found in the envelope:\
-          \n\n\t{round(total, 6)}\n"
+          \n\n\t{answer}\n"
     )
+
+
+def remove_piece_from_envelope(piece: int, envelope: Counter) -> None:
+    """Remove a piece of paper of the given size from the envelope.
+
+    NOTE This function modifies its input arguments.
+    """
+    if envelope[piece] == 1:
+        del envelope[piece]
+    else:
+        envelope[piece] -= 1
+
+
+def add_new_pieces_from_piece_to_envelope(piece: int, envelope: Counter) -> None:
+    """Add new pieces torn from the given piece to the envelope.
+
+    NOTE: This function modifies its input arguments.
+    """
+    size_to_add = piece // 2
+    while size_to_add >= 1:
+        envelope[size_to_add] += 1
+        size_to_add = size_to_add // 2
+
+
+def convert_counter_to_frozenset(counter: Counter) -> frozenset:
+    """Convert the Counter to a frozenset.
+
+    This is useful for hashing and memoization.
+    """
+    return frozenset(counter.items())
 
 
 if __name__ == "__main__":
